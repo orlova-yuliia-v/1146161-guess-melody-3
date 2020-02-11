6b5f26791591be68fe9e76b9421fd5e3327206f9
Юлия <orlova.yuliia.v@gmail.com>
2020-02-11 22:05:38 +0300

    Устанавливает Enzyme, добавляет обработчик клика для кнопки и обновляет снепшот тесты

--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -6,6 +6,7 @@ it(`WelcomeScreen render correctly`, () => {
   const tree = renderer
     .create(<WelcomeScreen
       errorsCount={3}
+      onWelcomeButtonClick={() => {}}
     />)
     .toJSON();
 
