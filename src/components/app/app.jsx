2f731b10ece4df896bee2b2c852a0bdb0678f95e
Юлия <orlova.yuliia.v@gmail.com>
2020-02-15 16:36:01 +0300

    Подключает и настраивает роутинг в App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
 import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
@@ -9,9 +10,23 @@ const welcomeButtonHandler = () => {};
 const App = (props) => {
   const {errorsCount} = props;
 
-  return <WelcomeScreen
-    errorsCount={errorsCount}
-    onWelcomeButtonClick={welcomeButtonHandler}/>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorsCount={errorsCount}
+            onWelcomeButtonClick={welcomeButtonHandler}/>
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
